312ed4fb711af12afcf2506a1659b8e8e6e2b7ac
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-07-13 14:26:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
-// const Ajv = require("ajv");
-// const ajv = new Ajv();
-
 try {
   // Read the JSON file path from the input
   const jsonFilePath = core.getInput("json-file");
@@ -11,22 +8,7 @@ try {
   // Read the JSON file content
   const jsonContent = fs.readFileSync(jsonFilePath, "utf8");
 
-  // Read the schema file path from the input
-  // const schemaFilePath = core.getInput("schema-file");
-
-  // Read the schema file content
-  // const schemaContent = fs.readFileSync(schemaFilePath, "utf8");
-  // const validate = ajv.compile(schemaContent);
-  // const valid = validate(jsonContent);
-  // if (!valid) console.log(validate.errors);
-
-  // Perform validation using your chosen JSON schema library
-  // Replace this section with your own validation logic
-
-  // Log the JSON content
-  // console.log("JSON Content:", jsonContent);
-
-  // Set the JSON content as an output
+ // Set the JSON content as an output
   core.setOutput("json", jsonContent);
 } catch (error) {
   core.setFailed(`Action failed with error: ${error}`);

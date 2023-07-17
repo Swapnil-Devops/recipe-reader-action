4ebc1b7c3777b806247ca784286ce06828a838a2
swapnil <swapnilgawali244@gmail.com>
2023-07-17 23:55:50 +0530

    added changes in index.test.js

--- index.test.js
+++ index.test.js
@@ -1,9 +1,6 @@
 const fs = require("fs");
-const {main}  = require("./index");
 const core = require("@actions/core");
-
-// jest.mock("fs");
-// jest.mock("@actions/core");
+const { main } = require("./index");
 
 describe("index.js", () => {
   afterEach(() => {
@@ -11,28 +8,28 @@ describe("index.js", () => {
   });
 
   test("should set the JSON content as an output", () => {
-    
-    const mockGetInput = jest.spyOn(core, "getInput");
-    const mockReadFileSync = jest.spyOn(fs, "readFileSync");
-    const mockSetOutput = jest.spyOn(core, "setOutput");
+    const mockGetInput = jest.spyOn(core, "getInput").mockImplementation();
+    const mockReadFileSync = jest
+      .spyOn(fs, "readFileSync")
+      .mockImplementation();
+    const mockSetOutput = jest.spyOn(core, "setOutput").mockImplementation();
 
     const jsonFilePath = "example.json";
     const jsonContent = '{"name": "John Doe"}';
 
     mockGetInput.mockReturnValueOnce(jsonFilePath);
     mockReadFileSync.mockReturnValueOnce(jsonContent);
-    
+
     main(core);
-    
+
     expect(mockGetInput).toHaveBeenCalledWith("json-file");
     expect(mockReadFileSync).toHaveBeenCalledWith(jsonFilePath, "utf8");
     expect(mockSetOutput).toHaveBeenCalledWith("json", jsonContent);
   });
 
   test("should set the failure message on error", () => {
-    
-    const mockGetInput = jest.spyOn(core, "getInput");
-    const mockSetFailed = jest.spyOn(core, "setFailed");
+    const mockGetInput = jest.spyOn(core, "getInput").mockImplementation();
+    const mockSetFailed = jest.spyOn(core, "setFailed").mockImplementation();
 
     const jsonFilePath = "example.json";
     const error = new Error("no such file or directory");
@@ -41,9 +38,9 @@ describe("index.js", () => {
     fs.readFileSync.mockImplementationOnce(() => {
       throw error;
     });
-    
+
     main(core);
-    
+
     expect(mockGetInput).toHaveBeenCalledWith("json-file");
     expect(mockSetFailed).toHaveBeenCalledTimes(1);
     expect(mockSetFailed).toHaveBeenCalledWith(
